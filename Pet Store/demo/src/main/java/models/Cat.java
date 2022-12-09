0e6370068c52901ff012a544db1d56d299cf62eb
martinapet <119624325+martinapet@users.noreply.github.com>
2022-12-09 01:26:13 +0100

    Update Cat.java

--- Pet Store/demo/src/main/java/models/Cat.java
+++ Pet Store/demo/src/main/java/models/Cat.java
@@ -2,18 +2,18 @@ package models;
 
 import java.util.Date;
 
-public class Cat extends Pet{
-    public Cat(User owner, String name, petType type, String description, long dateOfBirth) {
+public class Dog extends Pet{
+    public Dog(User owner, String name, petType type, String description, long dateOfBirth) {
         super(owner, name, type, description, dateOfBirth);
         //TODO Auto-generated constructor stub
     }
-
+    public int rating;
     @Override
     public int price() {
         // TODO Auto-generated method stub
-        //return super.price();
-        //age(years) * 1$.
-        return (int)((new Date(dateOfBirth).getTime()*1));
+        //super.price();
+        //(age(years) * 1$) + (1$ * rating)
+       return (int)((new Date(dateOfBirth).getTime()*1)+(1*rating));
     }
     
 }
