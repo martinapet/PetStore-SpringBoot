03cc55d4e6463b18d89b9b5e9456db7a0ed5dc14
martinapet <119624325+martinapet@users.noreply.github.com>
2022-12-09 01:25:11 +0100

    Update User.java

--- Pet Store/demo/src/main/java/models/User.java
+++ Pet Store/demo/src/main/java/models/User.java
@@ -1,14 +1,17 @@
 package models;
+import java.util.List;
+
 import javax.persistence.Entity;
-import javax.persistence.OneToOne;
+import jakarta.persistence.ManyToOne;
+import services.PetService;
 
 @Entity
 public class User {
-    private String firstName;
-    private String lastName;
+    private static String firstName;
+    private static String lastName;
     private String email;
     private int budget;
-    @OneToOne
+    @ManyToOne
     private Pet pets;
 
     public User(String firstName, String lastName, String email, int budget, Pet pets){
@@ -33,7 +36,7 @@ public class User {
     public String getEmail() {
         return email;
     }
-    public int getBudget() {
+    public static int getBudget() {
         return budget;
     }
     public void setFirstName(String firstName) {
@@ -59,7 +62,41 @@ public class User {
         this.pets = pets;
     }
 
-    
+    public static void buy(List<User> users){
+        for(User u: users){
+            try{
+                buyPet(u);
+            }
+            catch(Exception e){
+                System.out.println(e);
+            }
+        }
+        
+    }
 
-    
+
+    public static void buyPet(User user){
+        Pet freePet = new Pet(Pet.owner == null && (User.getBudget() >= Dog.price() || User.getBudget() >= Cat.price()));
+        //pet od listata pets sto nema owner i userot ima dovolen budget da ja plati cenata na toj pet
+        //let petWithoutOwner=new Pet(owner == null && owner.getBudget()>=price());
+
+        if(freePet == null)
+        {
+            throw new Exception("All pets have {} owners, so user: " +User.firstName +User.lastName + " cant buy a pet");
+        
+        }
+        freePet.owner = user;
+        
+        switch(freePet.type){
+            case(Cat):
+            System.out.println("Meow, cat" +Cat.name+ " has owner" +Pet.owner);
+            break;
+            case(Dog):
+            System.out.println("Woof, dog" +Dog.name+ " has owner" +Pet.owner);
+            break;
+            default:
+            System.out.println("Pet has been bought!!");
+            break;
+        }
+    }    
 }
