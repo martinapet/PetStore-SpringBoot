f5474fc366ce7c869bc432b5336885a687deac7e
martinapet <119624325+martinapet@users.noreply.github.com>
2022-12-11 16:34:39 +0100

    Add files via upload

---
+++ Pet Store/demo/src/test/java/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Pet (owner, name, type, description, dateOfBirth) VALUES ('Martina Petrovska', 'Charlie', 'Dog', 'This pet is a dog!', '06.12.2014');
+INSERT INTO Pet (owner, name, type, description, dateOfBirth) VALUES ('Simon Simonovski', 'Bruno', 'Dog', 'This pet is a dog!', '12.08.2020');
+INSERT INTO Pet (owner, name, type, description, dateOfBirth) VALUES ('Jana Janevska', 'Bela', 'Cat', 'This pet is a cat!', '06.07.2018');
+INSERT INTO Pet (owner, name, type, description, dateOfBirth) VALUES ('Ivan Ivanovski', 'Bak', 'Dog', 'This pet is a dog!', '03.04.2015');
+INSERT INTO Pet (owner, name, type, description, dateOfBirth) VALUES ('Dora Jovanovska', 'Una', 'Cat', 'This pet is a cat!', '22.09.2017');
