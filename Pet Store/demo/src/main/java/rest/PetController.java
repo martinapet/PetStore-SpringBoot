f8edf12c219f79ed5068cbac6cf7a32e4f63dd25
martinapet <119624325+martinapet@users.noreply.github.com>
2022-12-11 16:09:23 +0100

    Update PetController.java

--- Pet Store/demo/src/main/java/rest/PetController.java
+++ Pet Store/demo/src/main/java/rest/PetController.java
@@ -1,6 +1,9 @@
 package rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,13 +20,18 @@ public class PetController {
     @Autowired
     PetService petsService;
 
+    @GetMapping("/list")
+    public List<Pet> list(){
+        return petsService.list();
+    }
+    
     private Pet createPet(Pet pets){
         return petsService.addPet(pets);
     }
 
     @PostMapping
     public Pet createPet(@RequestBody @Valid PetCreateForm form) {
-       Pet pet = petsService.createPets(new Pet (form.getOwner(), form.getName(), form.getType(), form.getDescription(), form.getDateOfBirth()));
+       Pet pet = petsService.createPets(new Pet (form.getOwner(), form.getName(), form.getDescription(), form.getDateOfBirth()));
        return pet;
     }
 }
