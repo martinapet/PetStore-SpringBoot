eb660fb8d02d0b518b972a52dafec901b38d8b79
martinapet <119624325+martinapet@users.noreply.github.com>
2022-12-11 16:06:16 +0100

    Add files via upload

---
+++ Pet Store/demo/src/main/java/models/Buy.java
@@ -0,0 +1,24 @@
+package models;
+
+public class Buy {
+    public int buyPet(User user){
+        //Pet freePet = new Pet(Pet.owner == null && (User.getBudget()>= Dog.price() || User.getBudget() >= Cat.price()));
+        //pet kojsto nema owner
+        Pet freePet = new Pet(); 
+        Dog freeDog = new Dog();
+        Cat freeCat = new Cat();
+        // availablePet = new Pet(Pet.owner == null && Pet.owner.getBudget()>=Dog.dogPrice());
+        if(Pet.owner == null){
+            if(user.getBudget()>=freeDog.price()){
+                System.out.println("Woof, dog" +freeDog.name+ " has owner" +Pet.owner);
+            }
+            if(user.getBudget()>=freeCat.price()){
+                System.out.println("Meow, cat" +freeCat.name+ " has owner" +Pet.owner);
+            }
+            System.out.println("Pet has been bought!!");            
+        }
+        return 0;
+        
+    }
+    
+}
