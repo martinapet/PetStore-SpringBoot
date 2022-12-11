9c6d9ca97de67b27aafb652173e438cb4235b435
martinapet <119624325+martinapet@users.noreply.github.com>
2022-12-11 23:15:55 +0100

    Update Buy.java

--- Pet Store/demo/src/main/java/models/Buy.java
+++ Pet Store/demo/src/main/java/models/Buy.java
@@ -1,13 +1,14 @@
 package models;
 
 public class Buy {
-    public static int buyPet(User user){
+    public static int buyPet(User user, Pet pet){
         //Pet freePet = new Pet(Pet.owner == null && (User.getBudget()>= Dog.price() || User.getBudget() >= Cat.price()));
         //pet kojsto nema owner
         Pet freePet = new Pet(); 
         Dog freeDog = new Dog();
         Cat freeCat = new Cat();
         // availablePet = new Pet(Pet.owner == null && Pet.owner.getBudget()>=Dog.dogPrice());
+        if(user.getPets() == null && user.getBudget()>=pet.price())
         if(Pet.owner == null){
             if(user.getBudget()>=freeDog.price()){
                 System.out.println("Woof, dog" +freeDog.name+ " has owner" +Pet.owner);
