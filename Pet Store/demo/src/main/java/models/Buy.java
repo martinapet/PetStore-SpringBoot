1122aa10fdc0e9423eaad197ab6f76b9a47abdab
martinapet <119624325+martinapet@users.noreply.github.com>
2022-12-11 23:31:07 +0100

    Update Buy.java

--- Pet Store/demo/src/main/java/models/Buy.java
+++ Pet Store/demo/src/main/java/models/Buy.java
@@ -1,7 +1,7 @@
 package models;
 
 public class Buy {
-    public static int buyPet(User user, Pet pet){
+    public int buyPet(User user, Pet pet){
         //Pet freePet = new Pet(Pet.owner == null && (User.getBudget()>= Dog.price() || User.getBudget() >= Cat.price()));
         //pet kojsto nema owner
         Pet freePet = new Pet(); 
@@ -9,12 +9,12 @@ public class Buy {
         Cat freeCat = new Cat();
         // availablePet = new Pet(Pet.owner == null && Pet.owner.getBudget()>=Dog.dogPrice());
         if(user.getPets() == null && user.getBudget()>=pet.price())
-        if(Pet.owner == null){
+        if(pet.owner == null){
             if(user.getBudget()>=freeDog.price()){
-                System.out.println("Woof, dog" +freeDog.name+ " has owner" +Pet.owner);
+                System.out.println("Woof, dog" +freeDog.name+ " has owner" +pet.owner);
             }
             if(user.getBudget()>=freeCat.price()){
-                System.out.println("Meow, cat" +freeCat.name+ " has owner" +Pet.owner);
+                System.out.println("Meow, cat" +freeCat.name+ " has owner" +pet.owner);
             }
             System.out.println("Pet has been bought!!");            
         }
