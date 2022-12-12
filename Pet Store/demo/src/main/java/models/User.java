b29a811bc91adfdfe689850d853edb7b90e8b8d9
martinapet <119624325+martinapet@users.noreply.github.com>
2022-12-12 19:10:58 +0100

    Update User.java

--- Pet Store/demo/src/main/java/models/User.java
+++ Pet Store/demo/src/main/java/models/User.java
@@ -1,4 +1,5 @@
 package models;
+
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -14,7 +15,7 @@ public class User {
     @ManyToOne
     private Pet pets;
 
-    public User(String firstName, String lastName, String email, int budget, Pet pets){
+    public User(String firstName, String lastName, String email, int budget, Pet pets) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
@@ -22,58 +23,65 @@ public class User {
         this.pets = pets;
     }
 
-    
     public User() {
     }
-
 
     public String getFirstName() {
         return firstName;
     }
+
     public String getLastName() {
         return lastName;
     }
+
     public String getEmail() {
         return email;
     }
+
     public int getBudget() {
         return budget;
     }
+
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public void setBudget(int budget) {
         this.budget = budget;
     }
-
 
     public Pet getPets() {
         return pets;
     }
 
-
     public void setPets(Pet pets) {
         this.pets = pets;
     }
 
-    public void buy(List<User> users){
-        for(User u: users){
-            Pet p = u.getPets();
-            try{
-                Buy.buyPet(u,p);
-            }
-            catch(Exception e){
+    public void buy(List<User> users, Dog d, Cat c) {
+        for (User u : users) {
+            // Dog d = new Dog();
+            // Cat c = new Cat();
+            try {
+                // Buy.buyPet(u,p);
+                if (u.getBudget() >= c.price()) {
+                    System.out.println("Meow, cat" + c.name + " has owner" + c.owner);
+                }
+                if (u.getBudget() >= d.price()) {
+                    System.out.println("Woof, dog" + d.name + " has owner" + d.owner);
+                }
+            } catch (Exception e) {
                 System.out.println(e);
             }
         }
     }
-    
 
-    
 }
