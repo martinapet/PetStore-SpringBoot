e34d2fe2e5c4693d28e0e2cb278997acf100420c
martinapet <119624325+martinapet@users.noreply.github.com>
2022-12-12 19:10:29 +0100

    Add files via upload

---
+++ Pet Store/demo/src/main/java/services/BuyService.java
@@ -0,0 +1,33 @@
+package services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import models.Dog;
+import models.Pet;
+import models.User;
+
+public class BuyService {
+
+    @Autowired
+    private PetService petService;
+    private UserService userService;
+
+    public void buyPet() {
+        List<Pet> allPets = petService.list();
+        List<User> allUsers = userService.list();
+
+        for (User u : allUsers) {
+            for (Pet p : allPets) {
+                if (p.owner == null) { //ako pet e sloboden, t.e. ako nema sopstvenik
+                    if (u.getBudget() >= p.price()) { //proveruva dali user-ot ima dovolno budget za da go kupi milenikot
+                        System.out.println("Pet has been bought!!");
+
+                    }
+                }
+
+            }
+        }
+    }
+}
